9d6fbcf7e4185e232f43944f25ba05ee1417fc9e
‘lingdacong’ <2796009827@qq.com>
2023-07-03 18:02:35 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Counter from './modules/EpubView.svelte'
+</script>
+
+<main>
+ <EpubView/>
+</main>
+
+<style>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './app.css'
+import App from './App.svelte'
+//https://www.svelte.cn/
+
+const app = new App({
+  target: document.getElementById('app'),
+})
+
+export default app
